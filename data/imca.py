f81453be3a95c30f673473fdfbe24f320cf98fb2
Ilyes <ilyes.khemakhem@gmail.com>
2020-04-13 19:56:48 +0100

    add data files

---
+++ data/imca.py
@@ -0,0 +1,383 @@
+import numpy as np
+import torch
+from scipy.stats import ortho_group
+from sklearn.preprocessing import scale
+from torch.utils.data import Dataset
+
+
+# define Dataset objects
+class FCEDataset(Dataset):
+    def __init__(self, X, Y):
+        self.x = torch.from_numpy(X)
+        self.y = torch.from_numpy(Y)
+        self.len = self.x.shape[0]
+        self.data_dim = self.x.shape[1]
+        self.noise_dim = self.y.shape[1]
+
+    def get_dims(self):
+        return self.data_dim, self.noise_dim
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+
+class ConditionalFCEDataset(Dataset):
+    def __init__(self, X, Y, U):
+        self.x = torch.from_numpy(X)
+        self.y = torch.from_numpy(Y)
+        self.u = torch.from_numpy(U)
+        self.len = self.x.shape[0]
+        self.data_dim = self.x.shape[1]
+        self.noise_dim = self.y.shape[1]
+        self.aux_dim = self.u.shape[1]
+
+    def get_dims(self):
+        return self.data_dim, self.noise_dim, self.aux_dim
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index], self.u[index]
+
+
+class SingleDataset(Dataset):
+    def __init__(self, X):
+        self.x = torch.from_numpy(X)
+        self.len = self.x.shape[0]
+        self.data_dim = self.x.shape[1]
+        print('data loaded on {}'.format(self.x.device))
+
+    def get_dims(self):
+        return self.data_dim
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        return self.x[index]
+
+
+class DoubleDataset(Dataset):
+    def __init__(self, X, Y):
+        self.x = torch.from_numpy(X)
+        self.y = torch.from_numpy(Y)
+        self.len = self.x.shape[0]
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+
+
+def leaky_ReLU_1d(d, negSlope):
+    """
+    one dimensional implementation of leaky ReLU
+    """
+    if d > 0:
+        return d
+    else:
+        return d * negSlope
+
+
+leaky1d = np.vectorize(leaky_ReLU_1d)
+
+
+def leaky_ReLU(D, negSlope):
+    """
+    implementation of leaky ReLU activation function
+    """
+    assert negSlope > 0  # must be positive
+    return leaky1d(D, negSlope)
+
+
+def sigmoidAct(x):
+    """
+    one dimensional application of sigmoid activation function
+    """
+    return 1. / (1 + np.exp(-1 * x))
+
+
+def generateUniformMat(Ncomp, condT):
+    """
+    generate a random matrix by sampling each element uniformly at random
+    check condition number versus a condition threshold
+    """
+    A = np.random.uniform(0, 2, (Ncomp, Ncomp)) - 1
+    for i in range(Ncomp):
+        A[:, i] /= np.sqrt((A[:, i] ** 2).sum())
+
+    while np.linalg.cond(A) > condT:
+        # generate a new A matrix!
+        A = np.random.uniform(0, 2, (Ncomp, Ncomp)) - 1
+        for i in range(Ncomp):
+            A[:, i] /= np.sqrt((A[:, i] ** 2).sum())
+
+    return A
+
+
+def generateUniformMat_minMax(Ncomp, condT, minVal=.5, maxVal=1.5):
+    """
+    generate a random matrix by sampling each element uniformly at random
+    check condition number versus a condition threshold
+    """
+    A = np.random.uniform(minVal, maxVal, (Ncomp, Ncomp))
+    for i in range(Ncomp):
+        A[:, i] /= np.sqrt((A[:, i] ** 2).sum())
+
+    while np.linalg.cond(A) > condT:
+        # generate a new A matrix!
+        A = np.random.uniform(minVal, maxVal, (Ncomp, Ncomp))
+        for i in range(Ncomp):
+            A[:, i] /= np.sqrt((A[:, i] ** 2).sum())
+
+    return A
+
+
+def gen_nonstationary_data(Ncomp, Nlayer, Nsegment, NsegmentObs, source='Laplace', NonLin='leaky', negSlope=.2,
+                           Niter4condThresh=1e4):
+    """
+
+    generate multivariate data based on the non-stationary non-linear ICA model of Hyvarinen & Morioka (2016)
+
+    INPUT
+        - Ncomp: number of components (i.e., dimensionality of the data)
+        - Nlayer: number of non-linear layers!
+        - Nsegment: number of data segments to generate
+        - NsegmentObs: number of observations per segment
+        - source: either Laplace or Gaussian, denoting distribution for latent sources
+        - NonLin: linearity employed in non-linear mixing. Can be one of "leaky" = leakyReLU or "sigmoid"=sigmoid
+          Specifically for leaky activation we also have:
+              - negSlope: slope for x < 0 in leaky ReLU
+              - Niter4condThresh: number of random matricies to generate to ensure well conditioned
+    OUTPUT:
+      - output is a dictionary with the following values:
+          - sources: original non-stationary source
+          - obs: mixed sources
+          - labels: segment labels (indicating the non stationarity in the data)
+
+
+    """
+
+    # check input is correct
+    assert NonLin in ['leaky', 'sigmoid']
+
+    # generate non-stationary data:
+    Nobs = NsegmentObs * Nsegment  # total number of observations
+    labels = np.array([0] * Nobs)  # labels for each observation (populate below)
+
+    # generate data, which we will then modulate in a non-stationary manner:
+    if source == 'Laplace':
+        dat = np.random.laplace(0, 1, (Nobs, Ncomp))
+        dat = scale(dat)  # set to zero mean and unit variance
+    elif source == 'Gaussian':
+        dat = np.random.normal(0, 1, (Nobs, Ncomp))
+        dat = scale(dat)
+    else:
+        raise Exception("wrong source distribution")
+
+    # get modulation parameters
+    modMat = np.random.uniform(0, 1, (Ncomp, Nsegment))
+
+    # now we adjust the variance within each segment in a non-stationary manner
+    for seg in range(Nsegment):
+        segID = range(NsegmentObs * seg, NsegmentObs * (seg + 1))
+        dat[segID, :] = np.multiply(dat[segID, :], modMat[:, seg])
+        labels[segID] = seg
+
+    # now we are ready to apply the non-linear mixtures:
+    mixedDat = np.copy(dat)
+
+    # generate mixing matrices:
+    # will generate random uniform matrices and check their condition number based on following simulations:
+    condList = []
+    for i in range(int(Niter4condThresh)):
+        # A = np.random.uniform(0,1, (Ncomp, Ncomp))
+        A = np.random.uniform(1, 2, (Ncomp, Ncomp))  # - 1
+        for i in range(Ncomp):
+            A[:, i] /= np.sqrt((A[:, i] ** 2).sum())
+        condList.append(np.linalg.cond(A))
+
+    condThresh = np.percentile(condList, 15)  # only accept those below 25% percentile
+
+    # now we apply layers of non-linearity (just one for now!). Note the order will depend on natural of nonlinearity!
+    # (either additive or more general!)
+    mixingList = []
+    for l in range(Nlayer - 1):
+        # generate causal matrix first:
+        A = generateUniformMat(Ncomp, condThresh)
+        mixingList.append(A)
+
+        # we first apply non-linear function, then causal matrix!
+        if NonLin == 'leaky':
+            mixedDat = leaky_ReLU(mixedDat, negSlope)
+        elif NonLin == 'sigmoid':
+            mixedDat = sigmoidAct(mixedDat)
+        # apply mixing:
+        mixedDat = np.dot(mixedDat, A)
+
+    return {'source': dat, 'obs': mixedDat, 'labels': labels, 'mixing': mixingList, 'var': modMat}
+
+
+def gen_IMCA_data(Ncomp, Nlayer, Nsegment, NsegmentObs, BaseCovariance, NonLin='leaky', negSlope=.2,
+                  Niter4condThresh=1e4):
+    """
+    generate data from an IMCA model where latent sources follow a
+    MoG distribution conditional on each segment
+    Crucial difference is latents are NOT conditionally
+    independent (unless we specify that BaseCovariance is diagonal!)
+
+    INPUT
+        - Ncomp: number of components (i.e., dimensionality of the data)
+        - Nlayer: number of non-linear layers!
+        - Nsegment: number of data segments to generate
+        - NsegmentObs: number of observations per segment
+        - source: either Laplace or Gaussian, denoting distribution for latent sources
+        - BaseCovariance: base measure covariance (make diagonal for cond. indep latents)
+        - NonLin: linearity employed in non-linear mixing. Can be one of "leaky" = leakyReLU or "sigmoid"=sigmoid
+          Specifically for leaky activation we also have:
+            - negSlope: slope for x < 0 in leaky ReLU
+            - Niter4condThresh: number of random matricies to generate to ensure well conditioned
+    OUTPUT:
+      - output is a dictionary with the following values:
+        - sources: original non-stationary source
+        - obs: mixed sources
+        - labels: segment labels (indicating the non stationarity in the data)
+    """
+
+    # define some params
+    Nobs = NsegmentObs * Nsegment  # total number of observations
+    labels = np.array([0] * Nobs)  # labels for each observation (populate below)
+    latents = np.zeros((Nobs, Ncomp))
+
+    # generate segment variance for latent variables
+    modMat = np.random.uniform(0.01, 3, (Ncomp, Nsegment))
+
+    for i in range(Nsegment):
+        # define presicion for segment i
+        Pres_i = np.linalg.inv(BaseCovariance) + np.diag(1 / modMat[:, i])
+        latents[(i * NsegmentObs):((i + 1) * NsegmentObs), :] = np.random.multivariate_normal(mean=np.zeros((Ncomp,)),
+                                                                                              cov=np.linalg.inv(Pres_i),
+                                                                                              size=NsegmentObs)
+        labels[(i * NsegmentObs):((i + 1) * NsegmentObs)] = i
+
+    # now we are ready to apply the non-linear mixtures:
+    mixedDat = np.copy(latents)
+
+    # generate mixing matrices:
+    # now we apply layers of non-linearity (just one for now!). Note the order will depend on natural of nonlinearity!
+    # (either additive or more general!)
+    mixingList = []
+    for l in range(Nlayer - 1):
+        # generate causal matrix first:
+        A = ortho_group.rvs(Ncomp)  # generateUniformMat( Ncomp, condThresh )
+        mixingList.append(A)
+
+        # we first apply non-linear function, then causal matrix!
+        if NonLin == 'leaky':
+            mixedDat = leaky_ReLU(mixedDat, negSlope)
+        elif NonLin == 'sigmoid':
+            mixedDat = sigmoidAct(mixedDat)
+        # apply mixing:
+        mixedDat = np.dot(mixedDat, A)
+
+    return {'source': latents, 'obs': mixedDat, 'labels': labels, 'mixing': mixingList, 'var': modMat,
+            'BaseCovariance': BaseCovariance}
+
+
+def gen_TCL_data_ortho(Ncomp, Nlayer, Nsegment, NsegmentObs, source='Laplace', NonLin='leaky', negSlope=.2,
+                       Niter4condThresh=1e4):
+    """
+    generate multivariate data based on the non-stationary non-linear ICA model of Hyvarinen & Morioka (2016)
+
+    we generate mixing matrices using random orthonormal matrices
+
+    INPUT
+        - Ncomp: number of components (i.e., dimensionality of the data)
+        - Nlayer: number of non-linear layers!
+        - Nsegment: number of data segments to generate
+        - NsegmentObs: number of observations per segment
+        - source: either Laplace or Gaussian, denoting distribution for latent sources
+        - NonLin: linearity employed in non-linear mixing. Can be one of "leaky" = leakyReLU or "sigmoid"=sigmoid
+          Specifically for leaky activation we also have:
+            - negSlope: slope for x < 0 in leaky ReLU
+            - Niter4condThresh: number of random matricies to generate to ensure well conditioned
+    OUTPUT:
+      - output is a dictionary with the following values:
+        - sources: original non-stationary source
+        - obs: mixed sources
+        - labels: segment labels (indicating the non stationarity in the data)
+    """
+
+    # check input is correct
+    assert NonLin in ['leaky', 'sigmoid']
+
+    # generate non-stationary data:
+    Nobs = NsegmentObs * Nsegment  # total number of observations
+    labels = np.array([0] * Nobs)  # labels for each observation (populate below)
+
+    # generate data, which we will then modulate in a non-stationary manner:
+    if source == 'Laplace':
+        dat = np.random.laplace(0, 1, (Nobs, Ncomp))
+        dat = scale(dat)  # set to zero mean and unit variance
+    elif source == 'Gaussian':
+        dat = np.random.normal(0, 1, (Nobs, Ncomp))
+        dat = scale(dat)
+    else:
+        raise Exception("wrong source distribution")
+
+    # get modulation parameters
+    modMat = np.random.uniform(0.01, 3, (Ncomp, Nsegment))
+
+    # meanMat = np.random.uniform(0, 5, (Ncomp, Nsegment))
+    meanMat = np.zeros((Ncomp, Nsegment))
+    # now we adjust the variance within each segment in a non-stationary manner
+    for seg in range(Nsegment):
+        segID = range(NsegmentObs * seg, NsegmentObs * (seg + 1))
+        dat[segID, :] = np.multiply(dat[segID, :], modMat[:, seg])
+        dat[segID, :] = np.add(dat[segID, :], meanMat[:, seg])
+        labels[segID] = seg
+
+    # now we are ready to apply the non-linear mixtures:
+    mixedDat = np.copy(dat)
+
+    # generate mixing matrices:
+    # now we apply layers of non-linearity (just one for now!). Note the order will depend on natural of nonlinearity!
+    # (either additive or more general!)
+    mixingList = []
+    for l in range(Nlayer - 1):
+        # generate causal matrix first:
+        A = ortho_group.rvs(Ncomp)  # generateUniformMat( Ncomp, condThresh )
+        mixingList.append(A)
+
+        # we first apply non-linear function, then causal matrix!
+        if NonLin == 'leaky':
+            mixedDat = leaky_ReLU(mixedDat, negSlope)
+        elif NonLin == 'sigmoid':
+            mixedDat = sigmoidAct(mixedDat)
+        # apply mixing:
+        mixedDat = np.dot(mixedDat, A)
+
+    return {'source': dat, 'obs': mixedDat, 'labels': labels, 'mixing': mixingList, 'var': modMat}
+
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    # X = gen_nonstationary_data(Ncomp=3, Nsegment=5, Nlayer=3, NsegmentObs=100, NonLin='leaky', negSlope=.2, Niter4condThresh=1e4)
+
+    baseCov = np.array([[1, .6], [.6, 1]])
+    X = gen_IMCA_data(Ncomp=2, Nsegment=5, Nlayer=3, BaseCovariance=baseCov, NsegmentObs=100, NonLin='leaky',
+                      negSlope=.2, Niter4condThresh=1e4)
+
+    plt.scatter(X['source'][:, 0], X['source'][:, 1])
+    plt.title('correlated latents')
+
+    plt.scatter(X['obs'][:, 0], X['obs'][:, 1])
+    plt.title('correlated latents')
---
+++ data/utils.py
@@ -0,0 +1,65 @@
+import os
+
+import numpy as np
+
+
+def to_one_hot(x, m=None):
+    "batch one hot"
+    if type(x) is not list:
+        x = [x]
+    if m is None:
+        ml = []
+        for xi in x:
+            ml += [xi.max() + 1]
+        m = max(ml)
+    dtp = x[0].dtype
+    xoh = []
+    for i, xi in enumerate(x):
+        xoh += [np.zeros((xi.size, int(m)), dtype=dtp)]
+        xoh[i][np.arange(xi.size), xi.astype(np.int)] = 1
+    return xoh
+
+
+
+def one_hot_encode(labels, n_labels=10):
+    """
+    Transforms numeric labels to 1-hot encoded labels. Assumes numeric labels are in the range 0, 1, ..., n_labels-1.
+    """
+
+    assert np.min(labels) >= 0 and np.max(labels) < n_labels
+
+    y = np.zeros([labels.size, n_labels]).astype(np.float32)
+    y[range(labels.size), labels] = 1
+
+    return y
+
+
+def single_one_hot_encode(label, n_labels=10):
+    """
+    Transforms numeric labels to 1-hot encoded labels. Assumes numeric labels are in the range 0, 1, ..., n_labels-1.
+    """
+
+    assert label >= 0 and label < n_labels
+
+    y = np.zeros([n_labels]).astype(np.float32)
+    y[label] = 1
+
+    return y
+
+
+mnist_one_hot_transform = lambda label: single_one_hot_encode(label, n_labels=10)
+contrastive_one_hot_transform = lambda label: single_one_hot_encode(label, n_labels=2)
+
+
+def make_dir(dir_name):
+    if dir_name[-1] != '/':
+        dir_name += '/'
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    return dir_name
+
+
+def make_file(file_name):
+    if not os.path.exists(file_name):
+        open(file_name, 'a').close()
+    return file_name
