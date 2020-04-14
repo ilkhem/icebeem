91f23f527a442389cf0861a20759eb6722349550
piomonti <ricardo.monti08@gmail.com>
2020-04-14 09:15:13 +0100

    add ivae

---
+++ ivae/IVAE.py
@@ -0,0 +1,359 @@
+from numbers import Number
+
+import torch
+from torch import distributions as dist
+from torch import nn
+from torch.nn import functional as F
+
+
+def weights_init(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight.data)
+
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, n_layers, activation='none', slope=.1, device='cpu'):
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.n_layers = n_layers
+        self.device = device
+        if isinstance(hidden_dim, Number):
+            self.hidden_dim = [hidden_dim] * (self.n_layers - 1)
+        elif isinstance(hidden_dim, list):
+            self.hidden_dim = hidden_dim
+        else:
+            raise ValueError('Wrong argument type for hidden_dim: {}'.format(hidden_dim))
+
+        if isinstance(activation, str):
+            self.activation = [activation] * (self.n_layers - 1)
+        elif isinstance(activation, list):
+            self.hidden_dim = activation
+        else:
+            raise ValueError('Wrong argument type for activation: {}'.format(activation))
+
+        self._act_f = []
+        for act in self.activation:
+            if act == 'lrelu':
+                self._act_f.append(lambda x: F.leaky_relu(x, negative_slope=slope))
+            elif act == 'xtanh':
+                self._act_f.append(lambda x: self.xtanh(x, alpha=slope))
+            elif act == 'sigmoid':
+                self._act_f.append(F.sigmoid)
+            elif act == 'none':
+                self._act_f.append(lambda x: x)
+            else:
+                ValueError('Incorrect activation: {}'.format(act))
+
+        if self.n_layers == 1:
+            _fc_list = [nn.Linear(self.input_dim, self.output_dim)]
+        else:
+            _fc_list = [nn.Linear(self.input_dim, self.hidden_dim[0])]
+            for i in range(1, self.n_layers - 1):
+                _fc_list.append(nn.Linear(self.hidden_dim[i - 1], self.hidden_dim[i]))
+            _fc_list.append(nn.Linear(self.hidden_dim[self.n_layers - 2], self.output_dim))
+        self.fc = nn.ModuleList(_fc_list)
+        self.to(self.device)
+
+    @staticmethod
+    def xtanh(x, alpha=.1):
+        """tanh function plus an additional linear term"""
+        return x.tanh() + alpha * x
+
+    def forward(self, x):
+        h = x
+        for c in range(self.n_layers):
+            if c == self.n_layers - 1:
+                h = self.fc[c](h)
+            else:
+                h = self._act_f[c](self.fc[c](h))
+        return h
+
+
+class Dist:
+    def __init__(self):
+        pass
+
+    def sample(self, *args):
+        pass
+
+    def log_pdf(self, *args, **kwargs):
+        pass
+
+
+class Normal(Dist):
+    def __init__(self, device='cpu'):
+        super().__init__()
+        self.device = device
+        self.c = 2 * np.pi * torch.ones(1).to(self.device)
+        self._dist = dist.normal.Normal(torch.zeros(1).to(self.device), torch.ones(1).to(self.device))
+        self.name = 'gauss'
+
+    def sample(self, mu, v):
+        eps = self._dist.sample(mu.size()).squeeze()
+        scaled = eps.mul(v.sqrt())
+        return scaled.add(mu)
+
+    def log_pdf(self, x, mu, v, reduce=True, param_shape=None):
+        """compute the log-pdf of a normal distribution with diagonal covariance"""
+        if param_shape is not None:
+            mu, v = mu.view(param_shape), v.view(param_shape)
+        lpdf = -0.5 * (torch.log(self.c) + v.log() + (x - mu).pow(2).div(v))
+        if reduce:
+            return lpdf.sum(dim=-1)
+        else:
+            return lpdf
+
+    def log_pdf_full(self, x, mu, v):
+        """
+        compute the log-pdf of a normal distribution with full covariance
+        v is a batch of "pseudo sqrt" of covariance matrices of shape (batch_size, d_latent, d_latent)
+        mu is batch of means of shape (batch_size, d_latent)
+        """
+        batch_size, d = mu.size()
+        cov = torch.einsum('bik,bjk->bij', v, v)  # compute batch cov from its "pseudo sqrt"
+        assert cov.size() == (batch_size, d, d)
+        inv_cov = torch.inverse(cov)  # works on batches
+        c = d * torch.log(self.c)
+        # matrix log det doesn't work on batches!
+        _, logabsdets = self._batch_slogdet(cov)
+        xmu = x - mu
+        return -0.5 * (c + logabsdets + torch.einsum('bi,bij,bj->b', [xmu, inv_cov, xmu]))
+
+    def _batch_slogdet(self, cov_batch: torch.Tensor):
+        """
+        compute the log of the absolute value of determinants for a batch of 2D matrices. Uses torch.slogdet
+        this implementation is just a for loop, but that is what's suggested in torch forums
+        gpu compatible
+        """
+        batch_size = cov_batch.size(0)
+        signs = torch.empty(batch_size, requires_grad=False).to(self.device)
+        logabsdets = torch.empty(batch_size, requires_grad=False).to(self.device)
+        for i, cov in enumerate(cov_batch):
+            signs[i], logabsdets[i] = torch.slogdet(cov)
+        return signs, logabsdets
+
+
+class Laplace(Dist):
+    def __init__(self, device='cpu'):
+        super().__init__()
+        self.device = device
+        self._dist = dist.laplace.Laplace(torch.zeros(1).to(self.device), torch.ones(1).to(self.device) / np.sqrt(2))
+        self.name = 'laplace'
+
+    def sample(self, mu, b):
+        eps = self._dist.sample(mu.size())
+        scaled = eps.mul(b)
+        return scaled.add(mu)
+
+    def log_pdf(self, x, mu, b, reduce=True, param_shape=None):
+        """compute the log-pdf of a laplace distribution with diagonal covariance"""
+        if param_shape is not None:
+            mu, b = mu.view(param_shape), b.view(param_shape)
+        lpdf = -torch.log(2 * b) - (x - mu).abs().div(b)
+        if reduce:
+            return lpdf.sum(dim=-1)
+        else:
+            return lpdf
+
+
+class Bernoulli(Dist):
+    def __init__(self, device='cpu'):
+        super().__init__()
+        self.device = device
+        self._dist = dist.bernoulli.Bernoulli(0.5 * torch.ones(1).to(self.device))
+        self.name = 'bernoulli'
+
+    def sample(self, p):
+        eps = self._dist.sample(p.size())
+        return eps
+
+    def log_pdf(self, x, f, reduce=True, param_shape=None):
+        """compute the log-pdf of a laplace distribution with diagonal covariance"""
+        if param_shape is not None:
+            f = f.view(param_shape)
+        lpdf = x * torch.log(f) + (1 - x) * torch.log(1 - f)
+        if reduce:
+            return lpdf.sum(dim=-1)
+        else:
+            return lpdf
+
+
+class iVAE(nn.Module):
+    def __init__(self, latent_dim, data_dim, aux_dim, prior=None, decoder=None, encoder=None,
+                 n_layers=3, hidden_dim=50, activation='lrelu', slope=.1, device='cpu', anneal=False):
+        super().__init__()
+
+        self.data_dim = data_dim
+        self.latent_dim = latent_dim
+        self.aux_dim = aux_dim
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.activation = activation
+        self.slope = slope
+        self.anneal_params = anneal
+
+        if prior is None:
+            self.prior_dist = Normal(device=device)
+        else:
+            self.prior_dist = prior
+
+        if decoder is None:
+            self.decoder_dist = Normal(device=device)
+        else:
+            self.decoder_dist = decoder
+
+        if encoder is None:
+            self.encoder_dist = Normal(device=device)
+        else:
+            self.encoder_dist = encoder
+
+        # prior_params
+        self.prior_mean = torch.zeros(1).to(device)
+        self.logl = MLP(aux_dim, latent_dim, hidden_dim, n_layers, activation=activation, slope=slope, device=device)
+        # decoder params
+        self.f = MLP(latent_dim, data_dim, hidden_dim, n_layers, activation=activation, slope=slope, device=device)
+        self.decoder_var = .01 * torch.ones(1).to(device)
+        # encoder params
+        self.g = MLP(data_dim + aux_dim, latent_dim, hidden_dim, n_layers, activation=activation, slope=slope,
+                     device=device)
+        self.logv = MLP(data_dim + aux_dim, latent_dim, hidden_dim, n_layers, activation=activation, slope=slope,
+                        device=device)
+
+        self.apply(weights_init)
+
+        self._training_hyperparams = [1., 1., 1., 1., 1]
+
+    def encoder_params(self, x, u):
+        xu = torch.cat((x, u), 1)
+        g = self.g(xu)
+        logv = self.logv(xu)
+        return g, logv.exp()
+
+    def decoder_params(self, s):
+        f = self.f(s)
+        return f, self.decoder_var
+
+    def prior_params(self, u):
+        logl = self.logl(u)
+        return self.prior_mean, logl.exp()
+
+    def forward(self, x, u):
+        prior_params = self.prior_params(u)
+        encoder_params = self.encoder_params(x, u)
+        z = self.encoder_dist.sample(*encoder_params)
+        decoder_params = self.decoder_params(z)
+        return decoder_params, encoder_params, z, prior_params
+
+    def elbo(self, x, u):
+        decoder_params, (g, v), z, prior_params = self.forward(x, u)
+        log_px_z = self.decoder_dist.log_pdf(x, *decoder_params)
+        log_qz_xu = self.encoder_dist.log_pdf(z, g, v)
+        log_pz_u = self.prior_dist.log_pdf(z, *prior_params)
+
+        if self.anneal_params:
+            a, b, c, d, N = self._training_hyperparams
+            M = z.size(0)
+            log_qz_tmp = self.encoder_dist.log_pdf(z.view(M, 1, self.latent_dim), g.view(1, M, self.latent_dim),
+                                                   v.view(1, M, self.latent_dim), reduce=False)
+            log_qz = torch.logsumexp(log_qz_tmp.sum(dim=-1), dim=1, keepdim=False) - np.log(M * N)
+            log_qz_i = (torch.logsumexp(log_qz_tmp, dim=1, keepdim=False) - np.log(M * N)).sum(dim=-1)
+
+            return (a * log_px_z - b * (log_qz_xu - log_qz) - c * (log_qz - log_qz_i) - d * (
+                    log_qz_i - log_pz_u)).mean(), z
+
+        else:
+            return (log_px_z + log_pz_u - log_qz_xu).mean(), z
+
+    def anneal(self, N, max_iter, it):
+        thr = int(max_iter / 1.6)
+        a = 0.5 / self.decoder_var.item()
+        self._training_hyperparams[-1] = N
+        self._training_hyperparams[0] = min(2 * a, a + a * it / thr)
+        self._training_hyperparams[1] = max(1, a * .3 * (1 - it / thr))
+        self._training_hyperparams[2] = min(1, it / thr)
+        self._training_hyperparams[3] = max(1, a * .5 * (1 - it / thr))
+        if it > thr:
+            self.anneal_params = False
+
+
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+from scipy.stats import spearmanr
+
+
+def mean_corr_coef_np(x, y, method='pearson'):
+    """
+    A numpy implementation of the mean correlation coefficient metric.
+
+    :param x: numpy.ndarray
+    :param y: numpy.ndarray
+    :param method: str, optional
+            The method used to compute the correlation coefficients.
+                The options are 'pearson' and 'spearman'
+                'pearson':
+                    use Pearson's correlation coefficient
+                'spearman':
+                    use Spearman's nonparametric rank correlation coefficient
+    :return: float
+    """
+    d = x.shape[1]
+    if method == 'pearson':
+        cc = np.corrcoef(x, y, rowvar=False)[:d, d:]
+    elif method == 'spearman':
+        cc = spearmanr(x, y)[0][:d, d:]
+    else:
+        raise ValueError('not a valid method: {}'.format(method))
+    cc = np.abs(cc)
+    score = cc[linear_sum_assignment(-1 * cc)].mean()
+    return score
+
+
+import torch
+from torch.utils.data import Dataset
+
+
+class CustomSyntheticDataset(Dataset):
+    def __init__(self, X, U, device='cpu'):
+        self.device = device
+        self.x = torch.from_numpy(X)#.to(device)
+        self.u = torch.from_numpy(U)#.to(device)
+        self.len = self.x.shape[0]
+        self.aux_dim = self.u.shape[1]
+        self.data_dim = self.x.shape[1]
+        self.latent_dim = self.data_dim
+        self.nps = int(self.len / self.aux_dim)
+        #print('data loaded on {}'.format(self.x.device))
+
+    def get_dims(self):
+        return self.data_dim, self.latent_dim, self.aux_dim
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        return self.x[index], self.u[index]
+
+    def get_metadata(self):
+        return {'nps': self.nps,
+                'ns': self.aux_dim,
+                'n': self.len,
+                'data_dim': self.data_dim,
+                'aux_dim': self.aux_dim,
+                }
+
+def to_one_hot(x, m=None):
+    "batch one hot"
+    if type(x) is not list:
+        x = [x]
+    if m is None:
+        ml = []
+        for xi in x:
+            ml += [xi.max() + 1]
+        m = max(ml)
+    dtp = x[0].dtype
+    xoh = []
+    for i, xi in enumerate(x):
+        xoh += [np.zeros((xi.size, int(m)), dtype=dtp)]
+        xoh[i][np.arange(xi.size), xi.astype(np.int)] = 1
+    return xoh
---
+++ ivae/IVAE_wrapper.py
@@ -0,0 +1,63 @@
+import numpy as np
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+
+from IVAE import iVAE, CustomSyntheticDataset, to_one_hot
+
+
+def IVAE_wrapper(X, U, batch_size=256, max_iter=7e4, seed=0, n_layers=3, hidden_dim=20, lr=1e-3, cuda=True):
+    
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    device = torch.device('cuda:0' if cuda else 'cpu')
+    print('training on {}'.format(torch.cuda.get_device_name(device) if cuda else 'cpu'))
+
+    # load data
+    print('Creating shuffled dataset..')
+    dset = CustomSyntheticDataset(X.astype(np.float32), U.astype(np.float32), device)
+    loader_params = {'num_workers': 1, 'pin_memory': True} if cuda else {}
+    train_loader = DataLoader(dset, shuffle=True, batch_size=batch_size, **loader_params)
+    data_dim, latent_dim, aux_dim = dset.get_dims()
+    N = len(dset)
+    max_epochs = int(max_iter // len(train_loader) + 1)
+
+    # define model and optimizer
+    print('Defining model and optimizer..')
+    model = iVAE(latent_dim, data_dim, aux_dim, activation='lrelu', device=device,
+                 n_layers=n_layers, hidden_dim=hidden_dim)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=3, verbose=True)
+
+    # training loop
+    print("Training..")
+    it = 0
+    model.train()
+    while it < max_iter:
+        elbo_train = 0
+        epoch = it // len(train_loader) + 1
+        for _, (x, u) in enumerate(train_loader):
+            #x.to('cuda', non_blocking=True)
+            #u.to('cuda', non_blocking=True)
+            it += 1
+            optimizer.zero_grad()
+
+            x, u = x.to(device), u.to(device)
+
+            elbo, z_est = model.elbo(x, u)
+            elbo.mul(-1).backward()
+            optimizer.step()
+
+            elbo_train += -elbo.item()
+
+        elbo_train /= len(train_loader)
+
+        scheduler.step(elbo_train)
+        #print('epoch {}/{} \tloss: {}'.format(epoch, max_epochs, elbo_train))
+
+    Xt, Ut = dset.x, dset.u
+    decoder_params, encoder_params, z, prior_params = model(Xt, Ut)
+    params = {'decoder': decoder_params, 'encoder': encoder_params, 'prior': prior_params}
+
+    return z, model, params
