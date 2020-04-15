06f99c44c63dbc640048894c698f9727eb4fe42b
piomonti <ricardo.monti08@gmail.com>
2020-04-15 09:53:48 +0100

    add conditional dsm loss

---
+++ losses/dsm.py
@@ -0,0 +1,35 @@
+import torch
+import torch.autograd as autograd
+
+
+def dsm(energy_net, samples, sigma=1):
+    samples.requires_grad_(True)
+    vector = torch.randn_like(samples) * sigma
+    perturbed_inputs = samples + vector
+    logp = -energy_net(perturbed_inputs)
+    dlogp = sigma ** 2 * autograd.grad(logp.sum(), perturbed_inputs, create_graph=True)[0]
+    kernel = vector
+    loss = torch.norm(dlogp + kernel, dim=-1) ** 2
+    loss = loss.mean() / 2.
+
+    return loss
+
+def conditional_dsm(energy_net, samples, segLabels, energy_net_final_layer, sigma=1):
+    samples.requires_grad_(True)
+    vector = torch.randn_like(samples) * sigma
+    perturbed_inputs = samples + vector
+
+    d = samples.shape[-1]
+
+    # apply conditioning
+    logp = -energy_net(perturbed_inputs).view(-1, d*d)
+    logp = torch.mm( logp, energy_net_final_layer )
+    # take only relevant segment energy
+    logp = logp[ segLabels ]
+
+    dlogp = sigma ** 2 * autograd.grad(logp.sum(), perturbed_inputs, create_graph=True)[0]
+    kernel = vector
+    loss = torch.norm(dlogp + kernel, dim=-1) ** 2
+    loss = loss.mean() / 2.
+
+    return loss
