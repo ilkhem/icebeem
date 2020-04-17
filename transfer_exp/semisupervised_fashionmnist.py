9a6322b1f1656d88b119b201d0762bdb88bc5a55
Ilyes <ilyes.khemakhem@gmail.com>
2020-04-17 18:41:19 +0100

    wrap all in functions

--- transfer_exp/semisupervised_fashionmnist.py
+++ transfer_exp/semisupervised_fashionmnist.py
@@ -2,46 +2,43 @@
 #
 #
 
-import numpy as np 
-import os 
-import torch 
-from torchvision.datasets import MNIST, CIFAR10, FashionMNIST
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Subset
 import pickle
-from torch.utils.data.dataloader import default_collate
-from sklearn.svm import LinearSVC
-from sklearn.preprocessing import scale 
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LogisticRegression
+
+import numpy as np
+import torch
+import torchvision.transforms as transforms
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from torch.utils.data import DataLoader
+from torch.utils.data.dataloader import default_collate
+from torchvision.datasets import FashionMNIST
 
 # load in the required modules
 from models.refinenet_dilated_baseline import RefineNetDilated
 
+
 def semisupervised_fmnist():
-    class_model = LinearSVC #LogisticRegression
+    class_model = LinearSVC  # LogisticRegression
     test_size = .15
 
     ### ------ TRANSFER -----
     # load config
-    config = pickle.load( open('transfer_exp/config_file_fashionMNIST.p', 'rb' ) )
+    config = pickle.load(open('transfer_exp/config_file_fashionMNIST.p', 'rb'))
 
     expFolder = 'FashionMNISTPreTrain'
-    checkpoint = '' # '1000'
+    checkpoint = ''  # '1000'
 
-    check_path = expFolder + '/' +'checkpoint' + checkpoint + '_5000.pth'
+    check_path = expFolder + '/' + 'checkpoint' + checkpoint + '_5000.pth'
 
     # load in states
     ckp_path = 'run/logs/' + check_path
-    states = torch.load( ckp_path, map_location='cuda:0')
+    states = torch.load(ckp_path, map_location='cuda:0')
 
     # define score
     score = RefineNetDilated(config).to('cuda:0')
     score = torch.nn.DataParallel(score)
     score.load_state_dict(states[0])
-
 
     print('loaded energy network')
     # load the config
@@ -63,8 +60,7 @@ def semisupervised_fmnist():
             transforms.ToTensor()
         ])
 
-
-    test_dataset = FashionMNIST('run/datasets/FashionMNIST_test/', train=False, download=True, transform=tran_transform )
+    test_dataset = FashionMNIST('run/datasets/FashionMNIST_test/', train=False, download=True, transform=tran_transform)
 
     print('loaded test data')
 
@@ -72,68 +68,68 @@ def semisupervised_fmnist():
         modified_batch = []
         for item in batch:
             image, label = item
-            if label in range(8,10):
+            if label in range(8, 10):
                 modified_batch.append(item)
         return default_collate(modified_batch)
 
+    test_loader = DataLoader(test_dataset, batch_size=config.training.batch_size, shuffle=False, num_workers=1,
+                             drop_last=True, collate_fn=my_collate)
 
-    test_loader = DataLoader(test_dataset, batch_size=config.training.batch_size, shuffle=False, num_workers=1, drop_last=True, collate_fn = my_collate )
-
-    representations = np.zeros(( 10000, 28*28 ))
+    representations = np.zeros((10000, 28 * 28))
     labels = np.zeros((10000,))
     counter = 0
 
     for i, (X, y) in enumerate(test_loader):
-        #print( y )
-        #print( X.shape )
-        rep_i = score( X ).view(-1,28*28).data.cpu().numpy()
-        #print( rep_i.shape )
-        #print( rep_i.shape[0] )
-        representations[ counter:(counter+rep_i.shape[0]),: ] = rep_i
-        labels[ counter:(counter+rep_i.shape[0]) ] = y.data.numpy()
+        # print( y )
+        # print( X.shape )
+        rep_i = score(X).view(-1, 28 * 28).data.cpu().numpy()
+        # print( rep_i.shape )
+        # print( rep_i.shape[0] )
+        representations[counter:(counter + rep_i.shape[0]), :] = rep_i
+        labels[counter:(counter + rep_i.shape[0])] = y.data.numpy()
         counter += rep_i.shape[0]
-
 
     representations = representations[:counter, :]
     labels = labels[:counter]
     print('loaded representations')
 
-
     labels -= 8
-    rep_train, rep_test, lab_train , lab_test = train_test_split( (representations), labels, test_size=test_size, random_state=0)
+    rep_train, rep_test, lab_train, lab_test = train_test_split((representations), labels, test_size=test_size,
+                                                                random_state=0)
 
-    clf = class_model(random_state=0, max_iter=10000).fit( rep_train, lab_train )
-    acc = accuracy_score( lab_test, clf.predict( rep_test )) *100
+    clf = class_model(random_state=0, max_iter=10000).fit(rep_train, lab_train)
+    acc = accuracy_score(lab_test, clf.predict(rep_test)) * 100
 
-    print('Accuracy of transfer representation: acc={}'.format( np.round(acc,2)))
+    print('Accuracy of transfer representation: acc={}'.format(np.round(acc, 2)))
 
     ### ------ BASELINE -----
     # now repeat with the baseline of unconditional EBM
 
-    expFolder = 'FashionMNISTUncondBaseline' #'mnistBaseline' + str(ns)
-    checkpoint = '' # '1000'
+    expFolder = 'FashionMNISTUncondBaseline'  # 'mnistBaseline' + str(ns)
+    checkpoint = ''  # '1000'
 
-    check_path = expFolder + '/' +'checkpoint' + checkpoint + '_5000.pth'
+    check_path = expFolder + '/' + 'checkpoint' + checkpoint + '_5000.pth'
 
     # load in states
     ckp_path = 'run/logs/' + check_path
-    states = torch.load( ckp_path, map_location='cuda:0')
+    states = torch.load(ckp_path, map_location='cuda:0')
 
     # define score
     score_base = RefineNetDilated(config).to('cuda:0')
     score_base = torch.nn.DataParallel(score_base)
     score_base.load_state_dict(states[0])
 
-    test_loader = DataLoader(test_dataset, batch_size=config.training.batch_size, shuffle=False, num_workers=1, drop_last=True, collate_fn = my_collate )
+    test_loader = DataLoader(test_dataset, batch_size=config.training.batch_size, shuffle=False, num_workers=1,
+                             drop_last=True, collate_fn=my_collate)
 
-    representations_base = np.zeros(( 10000, 28*28 ))
+    representations_base = np.zeros((10000, 28 * 28))
     labels_base = np.zeros((10000,))
     counter = 0
 
     for i, (X, y) in enumerate(test_loader):
-        rep_i = score_base( X ).view(-1,28*28).data.cpu().numpy()
-        representations_base[ counter:(counter+rep_i.shape[0]),: ] = rep_i
-        labels_base[ counter:(counter+rep_i.shape[0]) ] = y.data.numpy()
+        rep_i = score_base(X).view(-1, 28 * 28).data.cpu().numpy()
+        representations_base[counter:(counter + rep_i.shape[0]), :] = rep_i
+        labels_base[counter:(counter + rep_i.shape[0])] = y.data.numpy()
         counter += rep_i.shape[0]
 
     representations_base = representations_base[:counter, :]
@@ -141,26 +137,10 @@ def semisupervised_fmnist():
     print('loaded representations')
 
     labels_base -= 8
-    rep_train_b, rep_test_b, lab_train_b , lab_test_b = train_test_split( (representations_base), labels_base, test_size=test_size, random_state=0)
+    rep_train_b, rep_test_b, lab_train_b, lab_test_b = train_test_split((representations_base), labels_base,
+                                                                        test_size=test_size, random_state=0)
 
-    clf_b = class_model(random_state=0, max_iter=10000).fit( rep_train_b, lab_train_b )
-    acc_b = accuracy_score( lab_test_b, clf_b.predict( rep_test_b )) *100
+    clf_b = class_model(random_state=0, max_iter=10000).fit(rep_train_b, lab_train_b)
+    acc_b = accuracy_score(lab_test_b, clf_b.predict(rep_test_b)) * 100
 
-    print('Accuracy of nonconditional representation learnt: acc={}'.format(np.round(acc_b,2)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print('Accuracy of nonconditional representation learnt: acc={}'.format(np.round(acc_b, 2)))
--- transfer_exp/transfer_nets.py
+++ transfer_exp/transfer_nets.py
@@ -2,277 +2,268 @@
 #
 #
 
-import numpy as np 
-import os 
-import torch 
-from torchvision.datasets import MNIST, CIFAR10, FashionMNIST
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Subset
+import os
 import pickle
+
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 from torch.utils.data.dataloader import default_collate
+from torchvision.datasets import MNIST, CIFAR10
 
 # load in the required modules
 from models.refinenet_dilated_baseline import RefineNetDilated
 
-import sys
-args = sys.argv
-SUBSET_SIZE = int(args[1])
-SEED        = int(args[2])
-DATASET     = int(args[3])
 
-if DATASET == 0:
-    DATASET = 'MNIST'
-else:
-    DATASET = 'CIFAR10'
+def transfer(args):
+    SUBSET_SIZE = args.SubsetSize
+    SEED = args.seed
+    DATASET = args.dataset.upper()
 
-print('DATASET: '+ DATASET + ' SUBSET SIZE: ' + str(SUBSET_SIZE) +'\tSEED: ' + str(SEED))
+    print('DATASET: ' + DATASET + ' SUBSET SIZE: ' + str(SUBSET_SIZE) + '\tSEED: ' + str(SEED))
 
-if DATASET=='MNIST':
-    # load config
-    config = pickle.load( open('transfer_exp/config_file.p', 'rb' ) )
+    if DATASET == 'MNIST':
+        # load config
+        config = pickle.load(open('transfer_exp/config_file.p', 'rb'))
 
-    expFolder = 'mnistPreTrain'
-    checkpoint = '' # '1000'
+        expFolder = 'mnistPreTrain'
+        checkpoint = ''  # '1000'
 
-    check_path = expFolder + '/' +'checkpoint' + checkpoint + '_3000.pth'
+        check_path = expFolder + '/' + 'checkpoint' + checkpoint + '_3000.pth'
 
-    # load in states
-    onCluster = True 
-    if onCluster:
-    	ckp_path = 'run/logs/' + check_path
-    	states = torch.load( ckp_path, map_location='cuda:0')
+        # load in states
+        onCluster = True
+        if onCluster:
+            ckp_path = 'run/logs/' + check_path
+            states = torch.load(ckp_path, map_location='cuda:0')
 
-    	# define score
-    	score = RefineNetDilated(config).to('cuda:0')
-    	score = torch.nn.DataParallel(score)
-    	score.load_state_dict(states[0])
+            # define score
+            score = RefineNetDilated(config).to('cuda:0')
+            score = torch.nn.DataParallel(score)
+            score.load_state_dict(states[0])
+        else:
+            ckp_path = '/home/projects/IMCA/ncsn-master/run/logs/' + check_path
+            states = torch.load(ckp_path, map_location='cpu')
+
+            # define score
+            score = RefineNetDilated(config).to('cpu')
+            score = torch.nn.DataParallel(score)
+            score.load_state_dict(states[0])
+
+        print('loaded energy network')
+        # load the config
+
+        # now load in the data
+        if config.data.random_flip is False:
+            tran_transform = test_transform = transforms.Compose([
+                transforms.Resize(config.data.image_size),
+                transforms.ToTensor()
+            ])
+        else:
+            tran_transform = transforms.Compose([
+                transforms.Resize(config.data.image_size),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.ToTensor()
+            ])
+            test_transform = transforms.Compose([
+                transforms.Resize(config.data.image_size),
+                transforms.ToTensor()
+            ])
+
+        test_dataset = MNIST('datasets/mnist_test', train=False, download=True, transform=test_transform)
+
+        # define the subset
+
+        id_range = list(range(SUBSET_SIZE))
+        testset_1 = torch.utils.data.Subset(test_dataset, id_range)
+
+        # dataloader = DataLoader( trainset_1, batch_size=config.training.batch_size, shuffle=True, num_workers=1 )
+
+        def my_collate(batch):
+            modified_batch = []
+            for item in batch:
+                image, label = item
+                if label in range(8, 10):
+                    modified_batch.append(item)
+            return default_collate(modified_batch)
+
+        test_loader = DataLoader(testset_1, batch_size=config.training.batch_size, shuffle=True, num_workers=1,
+                                 drop_last=True, collate_fn=my_collate)
+
+        print('loaded test data')
+
+        if onCluster == False:
+            config.device = 'cpu'
+
+        energy_net_finalLayer = torch.ones((config.data.image_size * config.data.image_size, 2)).to(config.device)
+        energy_net_finalLayer.requires_grad_()
+
+        from losses.dsm import conditional_dsm  # dsm_score_estimation
+        import torch.optim as optim
+
+        # define the optimizer
+        parameters = [energy_net_finalLayer]
+        optimizer = optim.Adam(parameters, lr=config.optim.lr, weight_decay=config.optim.weight_decay,
+                               betas=(config.optim.beta1, 0.999), amsgrad=config.optim.amsgrad)
+
+        # start optimizing!
+        step = 0
+        eCount = 10
+        for epoch in range(eCount):
+            print('epoch: ' + str(epoch))
+            counter = 0
+            loss_track = []
+            for i, (X, y) in enumerate(test_loader):
+                # print(step)
+                step += 1
+
+                X = X.to(config.device)
+                X = X / 256. * 255. + torch.rand_like(X) / 256.
+
+                # replace this with either dsm or dsm_conditional_score_estimation function !!
+                y = y - y.min()  # make zero indexed for conditional_dsm function
+                loss = conditional_dsm(score, X, y, energy_net_finalLayer, sigma=0.01)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                # tb_logger.add_scalar('loss', loss, global_step=step)
+                # print(loss.item())
+                # logging.info("step: {}, loss: {}, maxLabel: {}".format(step, loss.item(), y.max()))
+                loss_track.append(loss.item())
+
+        # save loss_track somewhere!
+        generateLangevinSamples = False
+
+        if generateLangevinSamples == False:
+            os.chdir('transfer_exp/transferRes')
+            pickle.dump(loss_track, open('TransferCDSM_Size' + str(SUBSET_SIZE) + "_Seed" + str(SEED) + '.p', 'wb'))
+        else:
+            pass
+
+    elif DATASET == 'CIFAR10':
+        # load config
+        config = pickle.load(open('transfer_exp/config_file_cifar.p', 'rb'))
+
+        expFolder = 'cifarPreTrain'
+        checkpoint = ''  # '1000'
+
+        check_path = expFolder + '/' + 'checkpoint' + checkpoint + '_3000.pth'
+
+        # load in states
+        onCluster = True
+        if onCluster:
+            ckp_path = 'run/logs/' + check_path
+            states = torch.load(ckp_path, map_location='cuda:0')
+
+            # define score
+            score = RefineNetDilated(config).to('cuda:0')
+            score = torch.nn.DataParallel(score)
+            score.load_state_dict(states[0])
+        else:
+            ckp_path = '/home/projects/IMCA/ncsn-master/run/logs/' + check_path
+            states = torch.load(ckp_path, map_location='cpu')
+
+            # define score
+            score = RefineNetDilated(config).to('cpu')
+            score = torch.nn.DataParallel(score)
+            score.load_state_dict(states[0])
+
+        print('loaded energy network')
+        # load the config
+
+        # now load in the data
+        if config.data.random_flip is False:
+            tran_transform = test_transform = transforms.Compose([
+                transforms.Resize(config.data.image_size),
+                transforms.ToTensor()
+            ])
+        else:
+            tran_transform = transforms.Compose([
+                transforms.Resize(config.data.image_size),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.ToTensor()
+            ])
+            test_transform = transforms.Compose([
+                transforms.Resize(config.data.image_size),
+                transforms.ToTensor()
+            ])
+
+        # test_dataset = MNIST('datasets/mnist_test', train=False, download=True, transform=test_transform)
+        test_dataset = CIFAR10('datasets/cifar10_test', train=False, download=True, transform=test_transform)
+
+        # define the subset
+
+        id_range = list(range(SUBSET_SIZE))
+        testset_1 = torch.utils.data.Subset(test_dataset, id_range)
+
+        # dataloader = DataLoader( trainset_1, batch_size=config.training.batch_size, shuffle=True, num_workers=1 )
+
+        def my_collate(batch):
+            modified_batch = []
+            for item in batch:
+                image, label = item
+                if label in range(8, 10):
+                    modified_batch.append(item)
+            return default_collate(modified_batch)
+
+        test_loader = DataLoader(testset_1, batch_size=config.training.batch_size, shuffle=True, num_workers=1,
+                                 drop_last=True, collate_fn=my_collate)
+
+        print('loaded test data')
+
+        if onCluster == False:
+            config.device = 'cpu'
+
+        energy_net_finalLayer = torch.ones((config.data.image_size * config.data.image_size, 2)).to(config.device)
+        energy_net_finalLayer.requires_grad_()
+
+        from losses.dsm import conditional_dsm  # dsm_score_estimation
+        import torch.optim as optim
+
+        # define the optimizer
+        parameters = [energy_net_finalLayer]
+        optimizer = optim.Adam(parameters, lr=config.optim.lr, weight_decay=config.optim.weight_decay,
+                               betas=(config.optim.beta1, 0.999), amsgrad=config.optim.amsgrad)
+
+        # start optimizing!
+        step = 0
+        eCount = 10
+        for epoch in range(eCount):
+            print('epoch: ' + str(epoch))
+            counter = 0
+            loss_track = []
+            for i, (X, y) in enumerate(test_loader):
+                # print(step)
+                # print(y.min())
+                step += 1
+
+                X = X.to(config.device)
+                X = X / 256. * 255. + torch.rand_like(X) / 256.
+
+                # replace this with either dsm or dsm_conditional_score_estimation function !!
+                y = y - y.min()  # make zero indexed for conditional_dsm function
+                loss = conditional_dsm(score, X, y, energy_net_finalLayer, sigma=0.01)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                # tb_logger.add_scalar('loss', loss, global_step=step)
+                # print(loss.item())
+                # logging.info("step: {}, loss: {}, maxLabel: {}".format(step, loss.item(), y.max()))
+                loss_track.append(loss.item())
+
+        # save loss_track somewhere!
+        generateLangevinSamples = False
+
+        if generateLangevinSamples == False:
+            os.chdir('transfer_exp/transferRes_cifar')
+            pickle.dump(loss_track,
+                        open('cifar_TransferCDSM_Size' + str(SUBSET_SIZE) + "_Seed" + str(SEED) + '.p', 'wb'))
+        else:
+            pass
+
     else:
-    	ckp_path = '/home/projects/IMCA/ncsn-master/run/logs/' + check_path
-    	states = torch.load( ckp_path, map_location='cpu')
-
-    	# define score
-    	score = RefineNetDilated(config).to('cpu')
-    	score = torch.nn.DataParallel(score)
-    	score.load_state_dict(states[0])
-
-    print('loaded energy network')
-    # load the config
-
-    # now load in the data 
-    if config.data.random_flip is False:
-        tran_transform = test_transform = transforms.Compose([
-            transforms.Resize(config.data.image_size),
-            transforms.ToTensor()
-        ])
-    else:
-        tran_transform = transforms.Compose([
-            transforms.Resize(config.data.image_size),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.ToTensor()
-        ])
-        test_transform = transforms.Compose([
-            transforms.Resize(config.data.image_size),
-            transforms.ToTensor()
-        ])
-
-    test_dataset = MNIST('datasets/mnist_test', train=False, download=True, transform=test_transform)
-
-    # define the subset
-
-    id_range = list(range(SUBSET_SIZE))
-    testset_1 = torch.utils.data.Subset(test_dataset, id_range)
-
-    #dataloader = DataLoader( trainset_1, batch_size=config.training.batch_size, shuffle=True, num_workers=1 )
-
-    def my_collate(batch):
-        modified_batch = []
-        for item in batch:
-            image, label = item
-            if label in range(8,10):
-                modified_batch.append(item)
-        return default_collate(modified_batch)
-
-
-    test_loader = DataLoader(testset_1, batch_size=config.training.batch_size, shuffle=True, num_workers=1, drop_last=True, collate_fn = my_collate )
-
-    print('loaded test data')
-
-    if onCluster == False:
-    	config.device = 'cpu'
-
-    energy_net_finalLayer = torch.ones((  config.data.image_size * config.data.image_size, 2 )).to(config.device)
-    energy_net_finalLayer.requires_grad_()
-
-    from losses.dsm import  conditional_dsm # dsm_score_estimation
-    import torch.optim as optim
-    import logging
-
-    # define the optimizer
-    parameters = [ energy_net_finalLayer ]
-    optimizer = optim.Adam(parameters, lr=config.optim.lr, weight_decay=config.optim.weight_decay,
-                                  betas=(config.optim.beta1, 0.999), amsgrad=config.optim.amsgrad)
-
-    # start optimizing!
-    step = 0
-    eCount = 10
-    for epoch in range( eCount ):
-        print('epoch: ' + str(epoch))
-        counter = 0
-        loss_track = []
-        for i, (X, y) in enumerate(test_loader):
-            #print(step)
-            step += 1
-
-            X = X.to(config.device)
-            X = X / 256. * 255. + torch.rand_like(X) / 256.
-
-            # replace this with either dsm or dsm_conditional_score_estimation function !!
-            y = y - y.min() # make zero indexed for conditional_dsm function
-            loss = conditional_dsm(score, X, y, energy_net_finalLayer,  sigma=0.01)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            #tb_logger.add_scalar('loss', loss, global_step=step)
-            #print(loss.item())
-            #logging.info("step: {}, loss: {}, maxLabel: {}".format(step, loss.item(), y.max()))
-            loss_track.append( loss.item() )
-
-
-
-    # save loss_track somewhere! 
-    generateLangevinSamples = False
-
-    if generateLangevinSamples==False:
-        os.chdir('transfer_exp/transferRes')
-        pickle.dump( loss_track, open('TransferCDSM_Size' + str(SUBSET_SIZE) + "_Seed" + str(SEED) + '.p', 'wb' ))
-    else:
-        pass
-
-
-if DATASET=='CIFAR10':
-    # load config
-    config = pickle.load( open('transfer_exp/config_file_cifar.p', 'rb' ) )
-
-    expFolder = 'cifarPreTrain'
-    checkpoint = '' # '1000'
-
-    check_path = expFolder + '/' +'checkpoint' + checkpoint + '_3000.pth'
-
-    # load in states
-    onCluster = True 
-    if onCluster:
-        ckp_path = 'run/logs/' + check_path
-        states = torch.load( ckp_path, map_location='cuda:0')
-
-        # define score
-        score = RefineNetDilated(config).to('cuda:0')
-        score = torch.nn.DataParallel(score)
-        score.load_state_dict(states[0])
-    else:
-        ckp_path = '/home/projects/IMCA/ncsn-master/run/logs/' + check_path
-        states = torch.load( ckp_path, map_location='cpu')
-
-        # define score
-        score = RefineNetDilated(config).to('cpu')
-        score = torch.nn.DataParallel(score)
-        score.load_state_dict(states[0])
-
-    print('loaded energy network')
-    # load the config
-
-    # now load in the data 
-    if config.data.random_flip is False:
-        tran_transform = test_transform = transforms.Compose([
-            transforms.Resize(config.data.image_size),
-            transforms.ToTensor()
-        ])
-    else:
-        tran_transform = transforms.Compose([
-            transforms.Resize(config.data.image_size),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.ToTensor()
-        ])
-        test_transform = transforms.Compose([
-            transforms.Resize(config.data.image_size),
-            transforms.ToTensor()
-        ])
-
-    #test_dataset = MNIST('datasets/mnist_test', train=False, download=True, transform=test_transform)
-    test_dataset = CIFAR10('datasets/cifar10_test', train=False, download=True, transform=test_transform)
-
-    # define the subset
-
-    id_range = list(range(SUBSET_SIZE))
-    testset_1 = torch.utils.data.Subset(test_dataset, id_range)
-
-    #dataloader = DataLoader( trainset_1, batch_size=config.training.batch_size, shuffle=True, num_workers=1 )
-
-    def my_collate(batch):
-        modified_batch = []
-        for item in batch:
-            image, label = item
-            if label in range(8,10):
-                modified_batch.append(item)
-        return default_collate(modified_batch)
-
-
-    test_loader = DataLoader(testset_1, batch_size=config.training.batch_size, shuffle=True, num_workers=1, drop_last=True, collate_fn = my_collate )
-
-    print('loaded test data')
-
-    if onCluster == False:
-        config.device = 'cpu'
-
-    energy_net_finalLayer = torch.ones((  config.data.image_size * config.data.image_size, 2 )).to(config.device)
-    energy_net_finalLayer.requires_grad_()
-
-    from losses.dsm import  conditional_dsm # dsm_score_estimation
-    import torch.optim as optim
-    import logging
-
-    # define the optimizer
-    parameters = [ energy_net_finalLayer ]
-    optimizer = optim.Adam(parameters, lr=config.optim.lr, weight_decay=config.optim.weight_decay,
-                                  betas=(config.optim.beta1, 0.999), amsgrad=config.optim.amsgrad)
-
-    # start optimizing!
-    step = 0
-    eCount = 10
-    for epoch in range( eCount ):
-        print('epoch: ' + str(epoch))
-        counter = 0
-        loss_track = []
-        for i, (X, y) in enumerate(test_loader):
-            #print(step)
-            #print(y.min())
-            step += 1
-
-            X = X.to(config.device)
-            X = X / 256. * 255. + torch.rand_like(X) / 256.
-
-            # replace this with either dsm or dsm_conditional_score_estimation function !!
-            y = y - y.min() # make zero indexed for conditional_dsm function
-            loss = conditional_dsm(score, X, y, energy_net_finalLayer,  sigma=0.01)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            #tb_logger.add_scalar('loss', loss, global_step=step)
-            #print(loss.item())
-            #logging.info("step: {}, loss: {}, maxLabel: {}".format(step, loss.item(), y.max()))
-            loss_track.append( loss.item() )
-
-    # save loss_track somewhere! 
-    generateLangevinSamples = False
-
-    if generateLangevinSamples==False:
-        os.chdir('transfer_exp/transferRes_cifar')
-        pickle.dump( loss_track, open('cifar_TransferCDSM_Size' + str(SUBSET_SIZE) + "_Seed" + str(SEED) + '.p', 'wb' ))
-    else:
-        pass
-
-
-
+        raise ValueError('Unknown dataset {}'.format(DATASET))
