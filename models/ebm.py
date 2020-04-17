37a9692fdaf861fd1cc1c3f9b0d3165775c69bf9
Ilyes <ilyes.khemakhem@gmail.com>
2020-04-17 15:14:36 +0100

    move VAT loss to fce

--- models/ebm.py
+++ models/ebm.py
@@ -1,9 +1,8 @@
-import contextlib
 import torch
 import torch.nn.functional as F
 from torch import nn
 
-from .nets import CleanMLP, MLP_general
+from .nets import CleanMLP
 
 
 class UnnormalizedConditialEBM(nn.Module):
@@ -87,70 +86,3 @@ class ModularConditionalEBM(ModularUnnormalizedConditionalEBM):
 
     def log_pdf(self, x, y, augment=True, positive=False):
         return super().log_pdf(x, y, augment, positive) + self.log_norm
-
-
-### Virtual adversarial regularization loss
-#
-#
-# this code has been shamelessly taken from:
-#  https://raw.githubusercontent.com/lyakaap/VAT-pytorch/master/vat.py
-#
-###
-
-
-@contextlib.contextmanager
-def _disable_tracking_bn_stats(model):
-    def switch_attr(m):
-        if hasattr(m, 'track_running_stats'):
-            m.track_running_stats ^= True
-
-    model.apply(switch_attr)
-    yield
-    model.apply(switch_attr)
-
-
-def _l2_normalize(d):
-    d_reshaped = d.view(d.shape[0], -1, *(1 for _ in range(d.dim() - 2)))
-    d /= torch.norm(d_reshaped, dim=1, keepdim=True) + 1e-8
-    return d
-
-
-class VATLoss(nn.Module):
-
-    def __init__(self, xi=10.0, eps=1.0, ip=1):
-        """VAT loss
-        :param xi: hyperparameter of VAT (default: 10.0)
-        :param eps: hyperparameter of VAT (default: 1.0)
-        :param ip: iteration times of computing adv noise (default: 1)
-        """
-        super(VATLoss, self).__init__()
-        self.xi = xi
-        self.eps = eps
-        self.ip = ip
-
-    def forward(self, model, x):
-        with torch.no_grad():
-            pred = F.softmax(model(x), dim=1)
-
-        # prepare random unit tensor
-        d = torch.rand(x.shape).sub(0.5).to(x.device)
-        d = _l2_normalize(d)
-
-        with _disable_tracking_bn_stats(model):
-            # calc adversarial direction
-            for _ in range(self.ip):
-                d.requires_grad_()
-                pred_hat = model(x + self.xi * d)
-                logp_hat = F.log_softmax(pred_hat, dim=1)
-                adv_distance = F.kl_div(logp_hat, pred, reduction='batchmean')
-                adv_distance.backward()
-                d = _l2_normalize(d.grad)
-                model.zero_grad()
-
-            # calc LDS
-            r_adv = d * self.eps
-            pred_hat = model(x + r_adv)
-            logp_hat = F.log_softmax(pred_hat, dim=1)
-            lds = F.kl_div(logp_hat, pred, reduction='batchmean')
-
-        return lds
--- models/fce.py
+++ models/fce.py
@@ -2,20 +2,20 @@
 #
 #
 
+import contextlib
 import time
 
 import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
-import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
+from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
-from data.imca import ContrastiveConditionalDataset, ConditionalDataset
-from data.imca import SimpleDataset, ContrastiveSimpleDataset
+from data.imca import ContrastiveConditionalDataset, ConditionalDataset, SimpleDataset, ContrastiveSimpleDataset
 from data.utils import to_one_hot
-from .ebm import VATLoss
 
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
@@ -655,3 +655,70 @@ class FCETrainer:
             raise ValueError('wrong network {}'.format(network))
 
         return recov
+
+
+### Virtual adversarial regularization loss
+#
+#
+# this code has been shamelessly taken from:
+#  https://raw.githubusercontent.com/lyakaap/VAT-pytorch/master/vat.py
+#
+###
+
+
+@contextlib.contextmanager
+def _disable_tracking_bn_stats(model):
+    def switch_attr(m):
+        if hasattr(m, 'track_running_stats'):
+            m.track_running_stats ^= True
+
+    model.apply(switch_attr)
+    yield
+    model.apply(switch_attr)
+
+
+def _l2_normalize(d):
+    d_reshaped = d.view(d.shape[0], -1, *(1 for _ in range(d.dim() - 2)))
+    d /= torch.norm(d_reshaped, dim=1, keepdim=True) + 1e-8
+    return d
+
+
+class VATLoss(nn.Module):
+
+    def __init__(self, xi=10.0, eps=1.0, ip=1):
+        """VAT loss
+        :param xi: hyperparameter of VAT (default: 10.0)
+        :param eps: hyperparameter of VAT (default: 1.0)
+        :param ip: iteration times of computing adv noise (default: 1)
+        """
+        super(VATLoss, self).__init__()
+        self.xi = xi
+        self.eps = eps
+        self.ip = ip
+
+    def forward(self, model, x):
+        with torch.no_grad():
+            pred = F.softmax(model(x), dim=1)
+
+        # prepare random unit tensor
+        d = torch.rand(x.shape).sub(0.5).to(x.device)
+        d = _l2_normalize(d)
+
+        with _disable_tracking_bn_stats(model):
+            # calc adversarial direction
+            for _ in range(self.ip):
+                d.requires_grad_()
+                pred_hat = model(x + self.xi * d)
+                logp_hat = F.log_softmax(pred_hat, dim=1)
+                adv_distance = F.kl_div(logp_hat, pred, reduction='batchmean')
+                adv_distance.backward()
+                d = _l2_normalize(d.grad)
+                model.zero_grad()
+
+            # calc LDS
+            r_adv = d * self.eps
+            pred_hat = model(x + r_adv)
+            logp_hat = F.log_softmax(pred_hat, dim=1)
+            lds = F.kl_div(logp_hat, pred, reduction='batchmean')
+
+        return lds
