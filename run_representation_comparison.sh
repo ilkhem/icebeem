fb7dacd619777153ca68485ce4594f9b1c4a6f52
piomonti <ricardo.monti08@gmail.com>
2020-04-29 10:05:50 +0100

    fix shell script

--- run_representation_comparison.sh
+++ run_representation_comparison.sh
@@ -1,9 +1,8 @@
 # run representation learning exps
 
-python3 main.py --dataset CIFAR10 --nSims 2 --config cifar.yaml --representation --retrainNets
-python3 main.py --dataset CIFAR10 --nSims 2 --config cifar.yaml --representation --baseline --retrainNets
+#python3 main.py --dataset CIFAR10 --nSims 2 --config cifar.yaml --representation --retrainNets
 
 
-#python3 main.py --dataset MNIST --nSims 2 --config mnist.yaml --representation 
+python3 main.py --dataset MNIST --nSims 2 --config mnist.yaml --representation 
 #python3 main.py --dataset MNIST --nSims 2 --config mnist.yaml --representation --baseline
 
