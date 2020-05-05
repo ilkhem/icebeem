6e02c101eca63a25d7a24b5254d6b18c3410ac0d
Ilyes <ilyes.khemakhem@gmail.com>
2020-05-05 13:17:37 +0100

    add cdsm function that takes a conditional energy

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,2 +1,2 @@
-from .dsm import conditional_dsm, dsm_score_estimation, dsm
+from .dsm import conditional_dsm, dsm_score_estimation, dsm, cdsm
 __all__ = ["dsm"]
--- losses/dsm.py
+++ losses/dsm.py
@@ -20,6 +20,27 @@ def dsm(energy_net, samples, sigma=1):
     return loss
 
 
+def cdsm(energy_net, samples, conditions, sigma=1.):
+    """
+    Conditional denoising score matching
+    :param energy_net: an energy network that takes x and y as input and outputs energy of shape (batch_size,)
+    :param samples: values of dependent variable x
+    :param conditions: values of conditioning variable y
+    :param sigma: noise level for dsm
+    :return: cdsm loss of shape (batch_size,)
+    """
+    samples.requires_grad_(True)
+    vector = torch.randn_like(samples) * sigma
+    perturbed_inputs = samples + vector
+    logp = -energy_net(perturbed_inputs, conditions)
+    assert logp.ndim == 1
+    dlogp = sigma ** 2 * autograd.grad(logp.sum(), perturbed_inputs, create_graph=True)[0]
+    kernel = vector
+    loss = torch.norm(dlogp + kernel, dim=-1) ** 2
+    loss = loss.mean() / 2.
+    return loss
+
+
 def conditional_dsm(energy_net, samples, segLabels, energy_net_final_layer, sigma=1):
     samples.requires_grad_(True)
     vector = torch.randn_like(samples) * sigma
